faedde84085c30c3ba96da1ae5b7b490ba6619a6
maxence d'Espeuilles <maxence@openaccess.fr>
2017-03-31 12:33:57 +0200

    first commit

---
+++ Command/OaRestoreCommand.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace Mdespeuilles\BackupMigrateBundle\Command;
+
+use BackupManager\Compressors\CompressorProvider;
+use BackupManager\Compressors\GzipCompressor;
+use BackupManager\Config\Config;
+use BackupManager\Databases\DatabaseProvider;
+use BackupManager\Databases\MysqlDatabase;
+use BackupManager\Filesystems\Destination;
+use BackupManager\Filesystems\FilesystemProvider;
+use BackupManager\Filesystems\LocalFilesystem;
+use BackupManager\Manager;
+use deit\compression\ZipArchive;
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class OaRestoreCommand extends ContainerAwareCommand
+{
+    protected function configure()
+    {
+        $this
+            ->setName('oa:restore')
+            ->setDescription('...')
+        ;
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $output->writeln('Start restore database');
+        $database = new Config([
+            'base' => [
+                'type' => 'mysql',
+                'host' => $this->getContainer()->getParameter('database_host'),
+                'port' => '3306',
+                'user' => $this->getContainer()->getParameter('database_user'),
+                'pass' => $this->getContainer()->getParameter('database_password'),
+                'database' => $this->getContainer()->getParameter('database_name'),
+                'singleTransaction' => false,
+                'ignoreTables' => [],
+            ]
+        ]);
+    
+        $storage = new Config([
+            'local' => [
+                'type' => 'Local',
+                'root' => $this->getContainer()->get('kernel')->getRootDir() . '/../private'
+            ]
+        ]);
+    
+        $filesystems = new FilesystemProvider($storage);
+        $filesystems->add(new LocalFilesystem);
+        $database = new DatabaseProvider($database);
+        $database->add(new MysqlDatabase);
+    
+        $compressors = new CompressorProvider;
+        $compressors->add(new GzipCompressor);
+    
+        $manager = new Manager($filesystems, $database, $compressors);
+    
+        $manager->makeRestore()->run('local', 'database.sql.gz', 'base', 'gzip');
+        $output->writeln('Done !');
+        
+        $output->writeln('Start restore files');
+        $zip = new ZipArchive($this->getContainer()->get('kernel')->getRootDir() . '/../private/uploads.zip');
+        $zip->extractTo($this->getContainer()->get('kernel')->getRootDir() . '/../web');
+        $output->writeln('Done !');
+    }
+
+}
---
+++ MdespeuillesBackupMigrateBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace Mdespeuilles\BackupMigrateBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class MdespeuillesBackupMigrateBundle extends Bundle
+{
+}
