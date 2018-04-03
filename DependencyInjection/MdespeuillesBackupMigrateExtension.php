d7aa836c06b231e057b25e663c865bba589335ee
Maxence d'Espeuilles <mdespeuilles@openaccess.fr>
2018-04-03 11:06:19 +0200

    Load services

--- DependencyInjection/MdespeuillesBackupMigrateExtension.php
+++ DependencyInjection/MdespeuillesBackupMigrateExtension.php
@@ -22,6 +22,9 @@ class MdespeuillesBackupMigrateExtension extends Extension
         $configuration = new Configuration();
         $config = $this->processConfiguration($configuration, $configs);
         
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
+        
         foreach ($config as $key => $value) {
             $container->setParameter('mdespeuilles_backup_migrate.'.$key, $value);
         }
